7a2c7426d6a334dfbc606d43f081719e50e8d47e
Lalit Bafna <lalit.divineinfosys@gmail.com>
2022-12-06 10:25:45 +0530

    Schedule task rectified

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             (new UpdateMemberStatus())->updateStatus();
         })->dailyAt('00:01')
-            ->config('panel.timezone');
+            ->timezone(config('panel.timezone'));
     }
 
     /**
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
